4a779db98094abfb7e0148bd9bfec39157502516
arifshah1106 <71922035+arifshah1106@users.noreply.github.com>
2020-10-23 13:18:49 +0530

    Updates

---
+++ precache-manifest.3af667f1198f2756a828fedd5187fef9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4062de4f093b0a962244a0e32a458e32",
+    "url": "/aurawebtech/index.html"
+  },
+  {
+    "revision": "88700d437a4baeef5eaf",
+    "url": "/aurawebtech/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "6d1f18a957632c838cea",
+    "url": "/aurawebtech/static/css/main.2cd65757.chunk.css"
+  },
+  {
+    "revision": "88700d437a4baeef5eaf",
+    "url": "/aurawebtech/static/js/2.f6669ec5.chunk.js"
+  },
+  {
+    "revision": "f3ff267e432687199a939b77440e2a44",
+    "url": "/aurawebtech/static/js/2.f6669ec5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d1f18a957632c838cea",
+    "url": "/aurawebtech/static/js/main.22d43682.chunk.js"
+  },
+  {
+    "revision": "6c1ee53878f2fd5d1976",
+    "url": "/aurawebtech/static/js/runtime-main.efa9b78a.js"
+  },
+  {
+    "revision": "bf04bf43a5227c8d51a3b381bc510c65",
+    "url": "/aurawebtech/static/media/About.bf04bf43.jpg"
+  },
+  {
+    "revision": "b94d9224aebac0d39b86644f3a6524a5",
+    "url": "/aurawebtech/static/media/Andr_dev.b94d9224.jpg"
+  },
+  {
+    "revision": "f5c052aa4ccd823c4aeaed5fe01ee61b",
+    "url": "/aurawebtech/static/media/App_dev.f5c052aa.jpg"
+  },
+  {
+    "revision": "bcf868fc73d21b8401b59a6eccc8764d",
+    "url": "/aurawebtech/static/media/Digital_marketing.bcf868fc.jpg"
+  },
+  {
+    "revision": "cc40c32840c545622e8f988332bd3155",
+    "url": "/aurawebtech/static/media/Marketing.cc40c328.jpg"
+  },
+  {
+    "revision": "a6d341041b5c081e4b044152ca7233eb",
+    "url": "/aurawebtech/static/media/Soft_dev.a6d34104.jpg"
+  },
+  {
+    "revision": "2698ba7510c0a2380ffb01a019432d76",
+    "url": "/aurawebtech/static/media/Work.2698ba75.jpg"
+  }
+]);
